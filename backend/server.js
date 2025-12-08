1e583e7883c1f499badedb1ba0feaf91d5adb36e
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-12-08 17:52:55 +0530

    mongodb import

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 // Start Server
 const app = require("./src/app");
+const connectDB = require("./src/db/db");
 require("dotenv").config();
+
+
+connectDB();
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
