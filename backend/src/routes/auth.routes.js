82b47189be88535b0eb04f4162d6a1a8cd080d26
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-12-09 13:01:17 +0530

    food partner rotes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,8 +3,14 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
 
+// User auth routes
 router.post("/user/register", authController.registerUser);
 router.post("/user/login", authController.loginUser);
 router.get("/user/logout", authController.logoutUser);
+
+// Food partner routes
+router.post("/food-partner/register", authController.registerFoodPartner);
+router.post("/food-partner/login", authController.loginFoodPartner);
+router.get("/food-partner/logout", authController.logoutFoodPartner);
 
 module.exports = router;
