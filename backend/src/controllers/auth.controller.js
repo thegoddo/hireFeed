55a269d556e526981dd25d1aa79fe59e6af7e85d
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-12-09 12:24:11 +0530

    login controller done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -42,7 +42,43 @@ async function registerUser(req, res) {
   });
 }
 
-async function loginUser(req, res) {}
+async function loginUser(req, res) {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({
+    email,
+  });
+
+  if (!user) {
+    res.status(400).json({
+      message: "Invalid email or password.",
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message: "Invalid email or Password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.SECRET
+  );
+
+  res.cookie("token", token);
+  res.status(200).json({
+    message: "user logged in successfully!",
+    user: {
+      _id: user._id,
+      email: user.email,
+      fullName: user.fullname,
+    },
+  });
+}
 
 module.exports = {
   registerUser,
