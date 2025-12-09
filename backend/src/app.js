f5b2af5d433c9478dab6c5e1da9c70d4b9ca7b98
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-12-09 20:12:56 +0530

    add food routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
+const foodRoutes = require("./routes/food.routes");
 
 const app = express();
 app.use(express.json());
@@ -12,5 +13,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/food", foodRoutes);
 
 module.exports = app;
