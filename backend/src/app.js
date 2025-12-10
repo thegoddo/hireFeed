c20aa24064423d5c3c77edca73a01065f1b97321
theGoddo <78879232+thegoddo@users.noreply.github.com>
2025-12-10 11:05:55 +0530

    security and food partner

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,18 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
 const foodRoutes = require("./routes/food.routes");
+const foodPartnerRoutes = require("./routes/food-partner.routes");
+const cors = require("cors");
 
 const app = express();
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -14,5 +22,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/food", foodRoutes);
+app.use("/api/food-partner", foodPartnerRoutes);
 
 module.exports = app;
